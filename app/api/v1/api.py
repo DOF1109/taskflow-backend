79348a9585d692719c866984a65289d7e068124f
DOF1109 <friasdaniel.1109@gmail.com>
2025-07-26 15:20:19 -0300

    crud implementado para taks

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import login, projects, tasks, dashboard
+from app.api.v1.endpoints import login, projects, tasks, dashboard, tags
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 api_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
+
+api_router.include_router(tags.router, prefix="/tags", tags=["tags"])
